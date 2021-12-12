4a3bf7aad1d452eead680c22b21e08a4eafae3f4
emilie carlsen <emil103d@stud.kea.dk>
2021-12-12 11:42:30 +0100

    Hent produkter ned

--- page-alle-produkter.php
+++ page-alle-produkter.php
@@ -20,6 +20,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header(); ?>
 
 <style>
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+#produkter-oversigt {
+	display: grid;
+	justify-content: center;
+	gap: 25px;
+	margin: auto;
+	grid-template-columns: 1fr 1fr 1fr;
+} 
+
+.enkelt_produkt{
+	max-width: 1200px;
+	display: grid;
+	text-align: center;
+	margin: auto;
+}
 </style>
 
 
@@ -48,6 +68,7 @@ get_header(); ?>
 
 let temp = document.querySelector("template");
 const liste = document.querySelector("#produkter-oversigt");
+let produkter = [];
 
 start();
 
@@ -59,27 +80,27 @@ start();
 	  async function getJson() {
 	const url = "https://www.amadeusnoah.dk/kea/semester_2/10_eksamensprojekt/siff_roeder_v2/wp-json/wp/v2/produkt/?per_page=100"; // indsæt url for at hente alle produkter 
 	let data = await fetch(url);
+	produkter = await data.json();
 	visProdukter();
 	  }
 
 	
 function visProdukter() { 
+	console.log("hej");
 	liste.textContent= "";
 	produkter.forEach(produkt => {
 		// if ((filterCat == "alle" || kursus.categories.includes(parseInt(filterCat))) && (filterTemaer == "alle" || kursus.temaer.includes(parseInt(filterTemaer)))) { //parseInt= laver datatypen om
 		// const klon = temp.cloneNode(true).content;
-		klon.querySelector("img").src = produkt.main_billede;
+		const klon = temp.cloneNode(true).content;
+		klon.querySelector("img").src = produkt.main_billede.guid;
 		klon.querySelector(".navn").textContent = produkt.title.rendered;
 		klon.querySelector(".koster").textContent = produkt.koster + " kr. ";
 		klon.querySelector("article").addEventListener("click", () => 
-			{location.href = kursus.link;
+			{location.href = produkt.link;
 			});
 		liste.appendChild(klon);
-		} 
 	});
 }
-
-
 
 </script>
 
